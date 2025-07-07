f7fc732635a8d49fde9b4c369838d4afe6ec7120
Juliana-Bispo <ju.souzbispo@gmail.com>
2025-07-07 15:09:26 -0300

    Delete processamento.hpp

--- processamento.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <opencv2/opencv.hpp>
-#include <string>
-
-std::string detectarCorPredominante(const cv::Mat& bgr_roi);
-std::string classificarMarcaPorCor(const std::string& cor);
